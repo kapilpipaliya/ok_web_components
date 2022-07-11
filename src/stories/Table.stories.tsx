641f7870ff5c55c21b6b82259d4654cfd1827929
kapil <kapil.pipaliya@yahoo.com>
2022-07-11 20:39:35 +0530

    add stories

---
+++ src/stories/Table.stories.tsx
@@ -0,0 +1,16 @@
+export default { title: "Application/Dashboard" };
+import { Story } from "@storybook/html";
+import { createSignal, For } from "solid-js";
+import { Table } from "ok_web_components";
+import { TableProps } from "../models/table";
+import { people } from "../utils/exampleData";
+
+const TableTemplate = (args: TableProps) => {
+  return <Table {...args} />;
+};
+
+export const ExampleTable = TableTemplate.bind({});
+
+ExampleTable.args = {
+  data: people,
+};
